945cd16170eaad6299aa436ed29faa4255f37c7a
AlisaCat <77342073+AlisaCat-S@users.noreply.github.com>
2023-03-15 22:37:00 +0800

    加注释

--- make_cbz.py
+++ make_cbz.py
@@ -3,11 +3,11 @@ import os
 import random
 import subprocess
 
-
 class ZipAndRenameFilesToCBZ:
     def __init__(self, directory):
         self.directory = directory
 
+        # 检查zip是否已安装
         cmd = ["dpkg", "-s", "zip"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
         if result.returncode != 0:
@@ -16,6 +16,7 @@ class ZipAndRenameFilesToCBZ:
         else:
             print("依赖:zip已安装！")
 
+        # 检查p7zip-full是否已安装
         cmd = ["dpkg", "-s", "p7zip-full"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
         if result.returncode != 0:
@@ -25,14 +26,18 @@ class ZipAndRenameFilesToCBZ:
             print("依赖:p7zip-full已安装！")
 
     def zip_folder(self, folder):
+        # 生成zip压缩包的名称
         zip_name = folder + ".zip"
         if os.path.exists(zip_name):
+            # 如果已经存在同名的zip文件，则在名称后加上随机数
             rand = random.randint(1, 1000)
             zip_name = folder + "_" + str(rand) + ".zip"
+        # 执行zip命令压缩文件夹
         cmd = ["zip", "-jr1", zip_name, folder]
         result = subprocess.run(cmd)
         if result.returncode == 0:
             print("压缩成功！[%s]" % zip_name)
+            # 删除原文件夹
             cmd = ['rm', '-rf', folder]
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
@@ -44,6 +49,7 @@ class ZipAndRenameFilesToCBZ:
             print("压缩失败！[%s]" % zip_name)
 
     def rename_files(self, old_ext, new_ext):
+        # 重命名指定扩展名的文件
         for file in os.listdir("."):
             if file.endswith(old_ext):
                 old_name = os.path.splitext(file)[0]
@@ -54,6 +60,7 @@ class ZipAndRenameFilesToCBZ:
                     print("压缩包重命名失败！[%s]" % file)
 
     def generate_random(self):
+        # 生成4位随机字符串
         rand = hashlib.md5(str(random.randint(1, 1000)).encode('utf-8')).hexdigest()[:4]
         return rand
 
@@ -61,19 +68,15 @@ class ZipAndRenameFilesToCBZ:
         if not os.path.exists(self.directory):
             print("工作目录不存在！")
             return
-
         os.chdir(self.directory)
-
         for folder in os.listdir("."):
             if os.path.isdir(folder):
                 self.zip_folder(folder)
-
         self.rename_files("zip", "cbz")
         self.rename_files("rar", "cbr")
         self.rename_files("7z", "cb7")
 
-
 if __name__ == "__main__":
-    directory = "Linux Path"
+    directory = "这里是工作路径"
     zip_and_rename_files = ZipAndRenameFilesToCBZ(directory)
     zip_and_rename_files.run()
